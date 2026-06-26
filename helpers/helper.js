4db2901bf8d76f41a94abd3c9daa9fe4438a5ebd
Muhamad Raafi Dewanto <rdewanto75@gmail.com>
2026-06-26 07:53:48 +0700

    done fix helper

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,22 +1,21 @@
 function getTimeAgo(createdAt) {
     let now = new Date()
     let createdDate = new Date(createdAt)
-    let a = now - createdAt
+    let a = now - createdDate
+
+    let detik = Math.max(1, Math.floor(a / 1000));
     let menit = Math.floor(a / (1000 * 60))
     let jam = Math.floor(a / (1000 * 60 * 60))
-    let detik = Math.floor(a % 60);
-
+    
+    if(detik < 60){
+        return `${detik} seconds ago`
+    } 
+    
     if(menit < 60){
         return `${menit} minutes ago`
     } 
     
-    if(menit < 1){
-        return `${detik} seconds ago`
-    } 
-    
-    if(menit > 60){
-        return `${jam} hours ago`
-    } 
+    return `${jam} hours ago`
     
 }
 
